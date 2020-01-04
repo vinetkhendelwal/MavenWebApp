8f463b46288119c2f929170d098b75ca38aba371
Vinet Khendelwal <vinetkhendelwal@gmail.com>
2020-01-04 13:38:19 +0530

    "Hello Git" - from eclipse
    Change-Id: I41c148587c5a7e0e6c508ef0480e3e195cab1d2b

---
+++ MavenWebApp/src/main/java/net/javaguides/maven_web_project/MavenWebApp/SimpleServlet.java
@@ -0,0 +1,22 @@
+package net.javaguides.maven_web_project.MavenWebApp;
+
+//package net.javaguides.mavenwebapp;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet("/hello")
+public class SimpleServlet extends HttpServlet {
+   private static final long serialVersionUID = 1L;
+   @Override
+   protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+         throws ServletException, IOException {
+      resp.setContentType("text/plain");
+      resp.getWriter().write("Hello World! Maven Web Project Example.");
+   }
+}
